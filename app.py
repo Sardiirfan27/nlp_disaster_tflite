7f1ba4b822282a19f655502931b6930bb600e8c6
sardiirfan27 <sardiirfan27@gmail.com>
2024-01-19 14:57:17 +0700

    version 4

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ import home
 import menu_app.tweet as tweet
 import analysis
 
-st.set_page_config(page_title='Disaster Tweet Analysis', page_icon=':bar_chart:', layout='wide',
-                   theme="light")
+st.set_page_config(page_title='Disaster Tweet Analysis', 
+                   page_icon=':bar_chart:', layout='wide',
+                   )
 
     
 # atur menu untuk app
